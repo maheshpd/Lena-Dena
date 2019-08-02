07d091672a09bb05db17ee873ede89ae3c1f1f7c
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-08-02 21:04:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/lenadena/activity/AddLena.java
@@ -0,0 +1,16 @@
+package com.example.lenadena.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lenadena.R;
+
+public class AddLena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_lena);
+    }
+}
