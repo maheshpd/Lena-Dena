4721784d946f3f134a93c0ca6f169935cf440644
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-08-02 23:43:24 +0530

    add daily expanse Design Done

---
+++ app/src/main/java/com/example/lenadena/activity/DailyExpense.java
@@ -0,0 +1,16 @@
+package com.example.lenadena.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lenadena.R;
+
+public class DailyExpense extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_expense);
+    }
+}
