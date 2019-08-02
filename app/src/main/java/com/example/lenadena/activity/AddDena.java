a2cfd8edb4762956548a8aadf57ade95fe984560
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-08-02 23:17:33 +0530

    add dena Design Done

---
+++ app/src/main/java/com/example/lenadena/activity/AddDena.java
@@ -0,0 +1,16 @@
+package com.example.lenadena.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lenadena.R;
+
+public class AddDena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dena);
+    }
+}
